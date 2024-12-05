8fde4e38270a8a040033d3a4feca9cbf1bad4db7
ArunKumar_D <arunkumard1018@gmail.com>
2024-12-05 23:47:49 +0530

    Implementing Invoice Config
    Implementing Invoice Schema

---
+++ server/src/model/InvoiceConfig.ts
@@ -0,0 +1,46 @@
+import mongoose, { InferSchemaType } from "mongoose";
+
+const invoiceConfigSchema = new mongoose.Schema({
+    inoviceheading: {
+        heading: { type: String },
+        subHeading: { type: String },
+        title: { type: String },
+    },
+    invoiceFrom: {
+        name: { type: String },
+        street: { type: String },
+        city: { type: String },
+        state: { type: String },
+        postalCode: { type: Number },
+        phone: { type: Number },
+    },
+    invoiceDetails: {
+        invoicePrefix: { type: String },
+        invoiceNo: { type: Number },
+        GSTIN: { type: String },
+        PAN: { type: String },
+        HSN: { type: Number },
+        stateCode: { type: Number },
+    },
+    bankDetails: {
+        bankName: { type: String },
+        accountName: { type: String },
+        accountNumber: { type: Number },
+        ifscCode: { type: String },
+        branch: { type: String },
+    },
+    additionlInfo: {
+        thankyouNote: { type: String },
+    },
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true },
+    business: { type: mongoose.Schema.Types.ObjectId, ref: "business", required: true },
+}, { timestamps: true })
+
+type InvoiceConfig = InferSchemaType<typeof invoiceConfigSchema>;
+type CreateInvoiceConfig = Omit<InvoiceConfig, 'createdAt' | 'updatedAt'>;
+
+/** InvoiceConfig Model */
+const InvoiceConfigModel = mongoose.model<InvoiceConfig>("invoiceConfig", invoiceConfigSchema);
+
+export { InvoiceConfig, InvoiceConfigModel, CreateInvoiceConfig };
+
---
+++ server/src/schemas/InvoiceConfigSchema.ts
@@ -0,0 +1,87 @@
+import Joi from "joi";
+
+/** Joi Schema for invoiceConfig validation */
+const invoiceConfigJoiSchema = Joi.object({
+    invoiceheading: Joi.object({
+        heading: Joi.string().required().messages({
+            "string.empty": "Heading is required",
+        }),
+        subHeading: Joi.string().allow(null, "").messages({
+            "string.base": "Subheading must be a string",
+        }),
+        title: Joi.string().allow(null, "").messages({
+            "string.base": "Title must be a string",
+        }),
+    }),
+
+    invoiceFrom: Joi.object({
+        name: Joi.string().required().messages({
+            "string.empty": "Name is required",
+        }),
+        street: Joi.string().required().messages({
+            "string.empty": "Street is required",
+        }),
+        city: Joi.string().required().messages({
+            "string.empty": "City is required",
+        }),
+        state: Joi.string().required().messages({
+            "string.empty": "State is required",
+        }),
+        postalCode: Joi.number().required().messages({
+            "number.base": "Postal code must be a number",
+            "any.required": "Postal code is required",
+        }),
+        phone: Joi.number().required().messages({
+            "number.base": "Phone number must be a number",
+            "any.required": "Phone number is required",
+        }),
+    }),
+
+    invoiceDetails: Joi.object({
+        invoicePrefix: Joi.string().required().messages({
+            "string.empty": "Invoice Prefix is required",
+        }),
+        invoiceNo: Joi.number().required().messages({
+            "number.base": "Invoice number must be a number",
+            "any.required": "Invoice number is required",
+        }),
+        GSTIN: Joi.string().allow(null, "").messages({
+            "string.base": "GSTIN must be a string",
+        }),
+        PAN: Joi.string().allow(null, "").messages({
+            "string.base": "PAN must be a string",
+        }),
+        HSN: Joi.number().allow(null).messages({
+            "number.base": "HSN must be a number",
+        }),
+        stateCode: Joi.number().allow(null).messages({
+            "number.base": "State code must be a number",
+        }),
+    }),
+
+    bankDetails: Joi.object({
+        bankName: Joi.string().allow(null, "").messages({
+            "string.base": "Bank name must be a string",
+        }),
+        accountName: Joi.string().allow(null, "").messages({
+            "string.base": "Account name must be a string",
+        }),
+        accountNumber: Joi.number().allow(null).messages({
+            "number.base": "Account number must be a number",
+        }),
+        ifscCode: Joi.string().allow(null, "").messages({
+            "string.base": "IFSC code must be a string",
+        }),
+        branch: Joi.string().allow(null, "").messages({
+            "string.base": "Branch name must be a string",
+        }),
+    }).optional(),
+
+    additionlInfo: Joi.object({
+        thankyouNote: Joi.string().allow(null, "").messages({
+            "string.base": "Thank you note must be a string",
+        }),
+    }).optional(),
+});
+
+export default invoiceConfigJoiSchema;
